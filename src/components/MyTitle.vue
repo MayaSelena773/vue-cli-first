b656b2ae143e6ea4f82d0c671dd7f83b53ea01af
MayaSelena773 <maya.bono721@gmail.com>
2022-07-05 17:48:38 +0200

    mytitledone

---
+++ src/components/MyTitle.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+import MyTitle from './components/MyTitle.vue'
+
+export default {
+  name: 'MyTitle',
+  components: {
+    MyTitle
+  }
+}
+</script>
+
+<style>
+
+h1 {
+    color: blueviolet
+}
+
+</style>
+
+
